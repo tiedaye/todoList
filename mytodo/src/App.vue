3b3734228e024fd03d45375b3a22a1b4b3a6939d
zhangshuang-h <781792980@qq.com>
2020-01-09 20:29:21 +0800

    建立vue项目

---
+++ mytodo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+<script>
+  import Home from '@/views/Home.vue'
+    export default {
+        props: {
+        },
+        components: {
+            Home
+        }
+    }
+</script>
+<style lang="scss">
+
+</style>
---
+++ mytodo/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ShoppingCart></ShoppingCart>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ShoppingCart from '@/components/ShoppingCart.vue'
+
+export default {
+  name: 'home',
+  components: {
+      ShoppingCart
+  }
+}
+</script>
